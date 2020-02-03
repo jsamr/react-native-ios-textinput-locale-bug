59861059a3e8bc22ba2dab3b25aaabe945809129
Jules Sam. Randolph <jules.sam.randolph@gmail.com>
2020-02-03 19:16:59 -0300

    initial comit

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {SafeAreaView, TextInput, StatusBar} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <TextInput />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
